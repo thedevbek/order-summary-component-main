81cb3889970441d4ef896097a7f868612f7edaaf
Bek Johansson <thedev.bek@gmail.com>
2023-10-19 23:56:40 -0500

    finished and updated README

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,13 @@ module.exports = {
 		fontFamily: {
 			sans: ['Red Hat Display', ['sans-serif']],
 		},
-        colors: {
-            'projectPaleBlue': ' hsl(225, 100%, 94%)',
-            'projectBrightBlue': ' hsl(245, 75%, 52%)',
-            'projectWhite': ' hsl(225, 100%, 98%)',
-            'projectDesaturatedBlue': 'hsl(224, 23%, 55%)',
-            'projectDarkBlue': ' hsl(223, 47%, 23%)'
-        }
+		colors: {
+			'color-pale-blue': ' hsl(225, 100%, 94%)',
+			'color-bright-blue': ' hsl(245, 75%, 52%)',
+			'color-white': ' hsl(225, 100%, 98%)',
+			'color-desaturated-blue': 'hsl(224, 23%, 55%)',
+			'color-dark-blue': ' rgb(203 213 225)',
+            'color-slate-blue': 'rgba(226 245 249)'
+		},
 	},
 };
